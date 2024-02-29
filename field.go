7c132ac86365ba30bccb70be289705aca456851d
ohzqq <iamchurkey@gmail.com>
2024-02-29 13:53:00 -0500

    ok

--- field.go
+++ field.go
@@ -226,23 +226,3 @@ func parseAttr(field *Field, attr string) {
 		i++
 	}
 }
-
-func parseQuery(query string) (string, string) {
-	var attr, q string
-	i := 0
-	for query != "" {
-		var a string
-		a, query, _ = strings.Cut(query, ":")
-		if a == "" {
-			continue
-		}
-		switch i {
-		case 0:
-			attr = a
-		case 1:
-			q = a
-		}
-		i++
-	}
-	return attr, q
-}
